a0bd18ebc16f2b46c679fc59b3ffc276b9ea6112
Eduardo Felipe <edu_felip@hotmail.com>
2021-08-13 02:43:07 -0300

    add swipe to delete

--- app/src/main/java/com/example/amazing_note/ui/fragments/ListFragment.kt
+++ app/src/main/java/com/example/amazing_note/ui/fragments/ListFragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.amazing_note.R
+import com.example.amazing_note.data.models.Note
 import com.example.amazing_note.databinding.FragmentListBinding
 import com.example.amazing_note.ui.adapters.ListAdapter
+import com.example.amazing_note.ui.utils.SwipeToDelete
 import com.example.amazing_note.ui.viewmodels.NoteViewModel
 import com.example.amazing_note.ui.viewmodels.SharedViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class ListFragment : Fragment() {
     private var _binding: FragmentListBinding? = null
@@ -51,6 +55,32 @@ class ListFragment : Fragment() {
         val recyclerView = binding.listfragRecyclerView
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        swipeToDelete(recyclerView)
+    }
+
+    private fun swipeToDelete(recyclerView: RecyclerView) {
+        val swipeToDeleteCallBack = object: SwipeToDelete() {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val noteDelete = adapter.noteList[viewHolder.adapterPosition]
+                mNoteViewModel.deleteNote(noteDelete)
+                adapter.notifyItemRemoved(viewHolder.adapterPosition)
+                restoreDeletedNote(viewHolder.itemView, noteDelete, viewHolder.adapterPosition)
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeToDeleteCallBack)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
+    private fun restoreDeletedNote(view: View, deletedNote: Note, position: Int) {
+        val snackBar = Snackbar.make(
+            view, "Deleted '${deletedNote.title}'", Snackbar.LENGTH_SHORT
+        )
+        snackBar.setAction("Undo") {
+            mNoteViewModel.insertNote(deletedNote)
+            adapter.notifyItemChanged(position)
+        }
+        snackBar.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
