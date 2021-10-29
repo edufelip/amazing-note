d4a7503fa21b24ea9f6247eb801ee091cba1ebf7
Eduardo Felipe <edu_felip@hotmail.com>
2021-10-29 10:55:46 -0300

    add dark mode

--- app/src/main/java/com/example/amazing_note/ui/fragments/ListFragment.kt
+++ app/src/main/java/com/example/amazing_note/ui/fragments/ListFragment.kt
@@ -1,11 +1,17 @@
 package com.example.amazing_note.ui.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.*
+import android.widget.CompoundButton
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat.recreate
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -15,6 +21,7 @@ import com.example.amazing_note.R
 import com.example.amazing_note.data.models.Note
 import com.example.amazing_note.databinding.FragmentListBinding
 import com.example.amazing_note.helpers.hideKeyboard
+import com.example.amazing_note.ui.MainActivity
 import com.example.amazing_note.ui.adapters.ListAdapter
 import com.example.amazing_note.ui.viewmodels.NoteViewModel
 import com.example.amazing_note.ui.viewmodels.SharedViewModel
@@ -40,6 +47,7 @@ class ListFragment : Fragment() {
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
         setHasOptionsMenu(true)
 
+        getThemePreferences()
         setNavDrawer()
         setupRecyclerView()
         setListeners()
@@ -70,16 +78,6 @@ class ListFragment : Fragment() {
                 adapter.setNotes(it)
             }
         })
-    }
-
-    private fun restoreDeletedNote(view: View, deletedNote: Note) {
-        val snackBar = Snackbar.make(
-            view, this.getString(R.string.deleted) + " '${deletedNote.title}'", Snackbar.LENGTH_SHORT
-        )
-        snackBar.setAction(this.getString(R.string.undo)) {
-            mNoteViewModel.insertNote(deletedNote)
-        }
-        snackBar.show()
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -114,6 +112,14 @@ class ListFragment : Fragment() {
                 searchDatabase(query.toString())
             }
         })
+
+        binding.lightDarkSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                setTheme("Dark")
+            } else {
+                setTheme("Light")
+            }
+        }
     }
 
     private fun setNavDrawer() {
@@ -130,6 +136,31 @@ class ListFragment : Fragment() {
                 }
                 else -> false
             }
+        }
+    }
+
+    private fun setTheme(theme: String) {
+        val preferences: SharedPreferences = requireContext().getSharedPreferences("Theme", Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = preferences.edit()
+        editor.putString("Theme", theme)
+        editor.apply()
+        changeTheme(theme)
+    }
+
+    private fun getThemePreferences() {
+        val sharedPreferences = requireContext().getSharedPreferences("Theme", Context.MODE_PRIVATE)
+        val theme = sharedPreferences.getString("Theme", "Default")
+        if(theme.isNullOrEmpty()) return
+        setTheme(theme)
+    }
+
+    private fun changeTheme(theme: String) {
+        when(theme) {
+            "Dark" -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.lightDarkSwitch.isChecked = true
+            }
+            "Light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 
